7b6ad09f3654bda328b2833adf4864a53d919ced
sbc4242 <bongchul@ualberta.ca>
2017-09-14 19:14:05 -0600

    Lab2_commit

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by bongchul on 9/14/17.
+ */
+
+public interface Tweetable {
+    public String getMessage();
+
+    public Date getDate();
+
+
+
+}
